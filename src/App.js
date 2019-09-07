7a1e1d20093be2e1b8054f65de549696ee6400e6
ssmith <shawn.t.smith.777@gmail.com>
2019-09-07 15:53:40 -0500

    cleaned up boiler plate a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>test</h1>
+      <h1>test again</h1>
     </div>
   );
 }
