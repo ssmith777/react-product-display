a52b69f1ae7a3ac20003c31259e63b06cf1b50a4
ssmith <shawn.t.smith.777@gmail.com>
2019-09-08 13:45:01 -0500

    corrected ReactBnbGallery state issue and corrected proper display of images select in gallery display

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -7,52 +7,62 @@ const photos = [
     photo: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
     // caption: 'Viñales, Pinar del Río, Cuba',
     // subcaption: 'Photo by Simon Matzinger on Unsplash',
-    thumbnail: 'https://i.ibb.co/CHLBZnp/gal2323.jpg'
+    thumbnail: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
+    id: 1
   },
   {
     photo: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
     // caption: 'La Habana, Cuba',
     // subcaption: 'Photo by Gerardo Sanchez on Unsplash',
-    thumbnail: 'https://i.ibb.co/4pBbhfY/gal39834.jpg'
+    thumbnail: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
+    id: 2
   },
   {
     photo: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
     // caption: 'Woman smoking a tobacco',
     // subcaption: 'Photo by Hannah Cauhepe on Unsplash',
-    thumbnail: 'https://i.ibb.co/xSnHP7g/gal43884.jpg'
+    thumbnail: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
+    id: 3
   },
   {
     photo: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
     // caption: 'Woman smoking a tobacco',
     // subcaption: 'Photo by Hannah Cauhepe on Unsplash',
-    thumbnail: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg'
+    thumbnail: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
+    id: 4
   },
   {
     photo: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
     // caption: 'Woman smoking a tobacco',
     // subcaption: 'Photo by Hannah Cauhepe on Unsplash',
-    thumbnail: 'https://i.ibb.co/dGZvj75/gal4545.jpg'
+    thumbnail: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
+    id: 5
   },
   {
     photo: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
     // caption: 'Woman smoking a tobacco',
     // subcaption: 'Photo by Hannah Cauhepe on Unsplash',
-    thumbnail: 'https://i.ibb.co/S6FVFNt/gal74744.jpg'
+    thumbnail: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
+    id: 6
   }
 ];
 
 class LightBox extends Component {
   constructor() {
     super(...arguments);
-    this.state = { galleryOpened: false, photoid: 0 };
+    this.state = {
+      galleryOpened: false,
+      photoid: 0
+    };
     this.toggleGallery = this.toggleGallery.bind(this);
   }
 
   toggleGallery = id => {
+    console.log(id);
     this.setState(prevState => ({
-      galleryOpened: !prevState.galleryOpened,
-      photoid: id
+      galleryOpened: !prevState.galleryOpened
     }));
+    this.setState({ photoid: id });
   };
 
   render() {
@@ -61,22 +71,58 @@ class LightBox extends Component {
         {/* <button onClick={this.toggleGallery}>Open photo gallery</button> */}
         <div className='section-c'>
           <div className='gallery'>
-            <Link to='' onClick={this.toggleGallery} className='big'>
+            <Link
+              to=''
+              onClick={() => {
+                this.toggleGallery(1);
+              }}
+              className='big'
+            >
               <img src='https://i.ibb.co/CHLBZnp/gal2323.jpg' alt='' />
             </Link>
-            <Link to='' onClick={this.toggleGallery} className='big'>
+            <Link
+              to=''
+              onClick={() => {
+                this.toggleGallery(2);
+              }}
+              className='big'
+            >
               <img src='https://i.ibb.co/4pBbhfY/gal39834.jpg' alt='' />
             </Link>
-            <Link to='' onClick={this.toggleGallery} className='big'>
+            <Link
+              to=''
+              onClick={() => {
+                this.toggleGallery(3);
+              }}
+              className='big'
+            >
               <img src='https://i.ibb.co/xSnHP7g/gal43884.jpg' alt='' />
             </Link>
-            <Link to='' onClick={this.toggleGallery} className='big'>
+            <Link
+              to=''
+              onClick={() => {
+                this.toggleGallery(4);
+              }}
+              className='big'
+            >
               <img src='https://i.ibb.co/QN6Bnrb/gal4958.jpg' alt='' />
             </Link>
-            <Link to='' onClick={this.toggleGallery} className='big'>
+            <Link
+              to=''
+              onClick={() => {
+                this.toggleGallery(5);
+              }}
+              className='big'
+            >
               <img src='https://i.ibb.co/dGZvj75/gal4545.jpg' alt='' />
             </Link>
-            <Link to='' onClick={this.toggleGallery} className='big'>
+            <Link
+              to=''
+              onClick={() => {
+                this.toggleGallery(6);
+              }}
+              className='big'
+            >
               <img src='https://i.ibb.co/S6FVFNt/gal74744.jpg' alt='' />
             </Link>
           </div>
@@ -85,6 +131,7 @@ class LightBox extends Component {
           show={this.state.galleryOpened}
           photos={photos}
           onClose={this.toggleGallery}
+          activePhotoIndex={this.state.photoid - 1}
         />
       </Fragment>
     );
