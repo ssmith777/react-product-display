b92e127d80c5035fc5c32e118c83baff9786f2e3
ssmith <shawn.t.smith.777@gmail.com>
2019-09-07 22:33:41 -0500

    added Gallery

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Showcase from './Showcase';
 import Overlay from './Overlay';
 import Footer from './Footer';
+import Gallery from './Gallery';
 
 const ProductPage = () => {
   return (
     <div>
       <Showcase />
       <Overlay />
+      <Gallery />
       <Footer />
     </div>
   );
