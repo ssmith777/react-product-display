e7bba9e50d8131409fa457c964488767c6f6e6c1
ssmith <shawn.t.smith.777@gmail.com>
2019-09-08 19:03:05 -0500

    Added PropTypes and default props to Lightbox.js, moved photos array to ProductPage.js to test props

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,58 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import ReactBnbGallery from 'react-bnb-gallery';
 import { Link } from 'react-router-dom';
-
-const photos = [
-  {
-    photo: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
-    caption: 'Jam out in style..',
-    subcaption: 'Jamming',
-    thumbnail: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
-    id: 1
-  },
-  {
-    photo: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
-    caption: 'Sleek style and color...',
-    subcaption: 'Sleek Design',
-    thumbnail: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
-    id: 2
-  },
-  {
-    photo: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
-    caption: 'Classic leather design...',
-    subcaption: 'Stylish Leather',
-    thumbnail: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
-    id: 3
-  },
-  {
-    photo: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
-    caption: 'Matches Apple and Windows products...',
-    subcaption: 'Apple White',
-    thumbnail: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
-    id: 4
-  },
-  {
-    photo: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
-    caption: 'Kid approved...',
-    subcaption: 'Rugged Design',
-    thumbnail: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
-    id: 5
-  },
-  {
-    photo: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
-    caption: 'Clean cut and sleek...',
-    subcaption: 'Sleek',
-    thumbnail: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
-    id: 6
-  }
-];
+import PropTypes from 'prop-types';
 
 class LightBox extends Component {
   constructor() {
     super(...arguments);
     this.state = {
       galleryOpened: false,
-      photoid: 0
+      photoid: 1
     };
     this.toggleGallery = this.toggleGallery.bind(this);
   }
@@ -65,12 +21,14 @@ class LightBox extends Component {
   };
 
   render() {
+    const { photos } = this.props;
     return (
       <Fragment>
         <div className='section-c'>
           <div className='gallery'>
             {photos.map(photo => (
               <Link
+                key={photo.id}
                 to=''
                 onClick={() => {
                   this.toggleGallery(photo.id);
@@ -86,10 +44,69 @@ class LightBox extends Component {
           show={this.state.galleryOpened}
           photos={photos}
           onClose={this.toggleGallery}
-          activePhotoIndex={this.state.photoid - 1}
+          activePhotoIndex={this.state.photoid}
         />
       </Fragment>
     );
   }
 }
 export default LightBox;
+
+LightBox.defaultProps = {
+  photos: [
+    {
+      photo: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
+      caption: 'Jam out in style..',
+      subcaption: 'Jamming',
+      thumbnail: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
+      id: 0
+    },
+    {
+      photo: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
+      caption: 'Sleek style and color...',
+      subcaption: 'Sleek Design',
+      thumbnail: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
+      id: 1
+    },
+    {
+      photo: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
+      caption: 'Classic leather design...',
+      subcaption: 'Stylish Leather',
+      thumbnail: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
+      id: 2
+    },
+    {
+      photo: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
+      caption: 'Matches Apple and Windows products...',
+      subcaption: 'Apple White',
+      thumbnail: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
+      id: 3
+    },
+    {
+      photo: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
+      caption: 'Kid approved...',
+      subcaption: 'Rugged Design',
+      thumbnail: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
+      id: 4
+    },
+    {
+      photo: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
+      caption: 'Clean cut and sleek...',
+      subcaption: 'Sleek',
+      thumbnail: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
+      id: 5
+    }
+  ]
+};
+
+LightBox.propTypes = {
+  photos: PropTypes.arrayOf(
+    PropTypes.shape({
+      photo: PropTypes.string.isRequired,
+      caption: PropTypes.string.isRequired,
+      subcaption: PropTypes.string.isRequired,
+      thumbnail: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -4,13 +4,57 @@ import Overlay from './Overlay';
 import Footer from './Footer';
 
 import Lightbox from './Lightbox';
+const photos = [
+  {
+    photo: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
+    caption: 'Jam out in style..',
+    subcaption: 'Jamming',
+    thumbnail: 'https://i.ibb.co/CHLBZnp/gal2323.jpg',
+    id: 0
+  },
+  {
+    photo: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
+    caption: 'Sleek style and color...',
+    subcaption: 'Sleek Design',
+    thumbnail: 'https://i.ibb.co/4pBbhfY/gal39834.jpg',
+    id: 1
+  },
+  {
+    photo: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
+    caption: 'Classic leather design...',
+    subcaption: 'Stylish Leather',
+    thumbnail: 'https://i.ibb.co/xSnHP7g/gal43884.jpg',
+    id: 2
+  },
+  {
+    photo: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
+    caption: 'Matches Apple and Windows products...',
+    subcaption: 'Apple White',
+    thumbnail: 'https://i.ibb.co/QN6Bnrb/gal4958.jpg',
+    id: 3
+  },
+  {
+    photo: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
+    caption: 'Kid approved...',
+    subcaption: 'Rugged Design',
+    thumbnail: 'https://i.ibb.co/dGZvj75/gal4545.jpg',
+    id: 4
+  },
+  {
+    photo: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
+    caption: 'Clean cut and sleek...',
+    subcaption: 'Sleek',
+    thumbnail: 'https://i.ibb.co/S6FVFNt/gal74744.jpg',
+    id: 5
+  }
+];
 
-const ProductPage = () => {
+const ProductPage = props => {
   return (
     <div>
       <Showcase />
       <Overlay />
-      <Lightbox />
+      <Lightbox photos={photos} />
       <Footer />
     </div>
   );
